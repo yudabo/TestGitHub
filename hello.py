0954d914229efd8c84ed39c30ffd9490cbb4f49a
yudabo <yudabo872178@163.com>
2019-08-25 20:37:17 +0800

    commit by yudabo 20190825

---
+++ hello.py
@@ -0,0 +1,10 @@
+def hello():
+    print ('hello Python!')
+if __name__ == "__main__":
+    hello()
+    print('运行部分代码示例！')
+    print('debug：step first')
+    print('debug：step second')
+    print('debug：step third')
+
+
---
+++ testGit.py
@@ -0,0 +1,4 @@
+def testGit():
+    print('this is a test method')
+if __name__ == "__main__":
+    testGit()
